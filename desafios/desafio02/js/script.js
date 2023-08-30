7806a4bd9ec313890a4b761aeb1fd470e03f38df
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-08-29 22:00:21 -0300

    desafios 2 e 3 finalizados

---
+++ desafios/desafio02/js/script.js
@@ -0,0 +1,5 @@
+function boasVindas(){
+    var nome = window.prompt('Qual é o seu nome?')
+    var idade = parseInt(window.prompt('Qual é a sua idade?'))
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
+}
---
+++ desafios/desafio03/js/script.js
@@ -0,0 +1,4 @@
+function antesDepois(){
+    var usuario = parseInt(window.prompt('Digite um número inteiro qualquer: '))
+    window.alert(`Antes de ${usuario}, temos o ${usuario - 1}\nDepois de ${usuario}, temos o ${usuario +1}`);
+}
