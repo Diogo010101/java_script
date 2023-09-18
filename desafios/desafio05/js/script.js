45e76b3841a07c7972394febf6e6d350e6a0a8d9
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-09-17 21:14:59 -0300

    desafio 05 JS finalizado conversor de metros

--- desafios/desafio05/js/script.js
+++ desafios/desafio05/js/script.js
@@ -1,8 +1,24 @@
 
 
 function converterFunction(){
-    var objMetros = parseFloat(window.prompt('Digite a distância em (m)'))
+    var objMetros = parseFloat(window.prompt('Digite a distância em (m)'));
+    var objSaida = document.querySelector("#saida1");
+    var objSaida2 = document.querySelector("#saida2");
     var km = objMetros / 1000;
-    document.write(`${objMetros} convertido em KM é igual a ${km.toFixed(2)}km`);
+    var hm = objMetros / 100;
+    var dam = objMetros / 10;
+    var dm = objMetros * 10;
+    var cm = objMetros * 100;
+    var mm = objMetros * 1000;
+
+    objSaida.innerHTML = `A distância de ${objMetros.toFixed(2).replace('.',',')} metros, corresponde a...`;
+    objSaida2.innerHTML = 
+           `${km.toFixed(3).replace('.',',')} Quilômetros (km) </br>
+            ${hm.toFixed(3).replace('.',',')} Hectômetros (Hm) <br>
+            ${dam.toFixed(2).replace('.',',')} Decâmetros (Dam) <br>
+            ${dm.toFixed(2).replace('.',',')} Decímetros (dm) <br>
+            ${cm.toFixed(2).replace('.',',')} Centímetros (cm) <br>
+            ${mm.toFixed(3)} Milímetros (mm) <br>
+        `;
 
 }
