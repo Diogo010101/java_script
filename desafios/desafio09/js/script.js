47cda33463ca74f6e552eb302c864be0d4b3a75b
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-09-24 18:49:30 -0300

    desafios 08 09 completos

---
+++ desafios/desafio09/js/script.js
@@ -0,0 +1,17 @@
+function reajusteSalario(){
+    var nome = window.prompt("Qual o nome do funcionário?");
+    var salario = parseFloat(window.prompt('Qual o salario do funcionario, utilize apenas números e ponto (.)'));
+    var porcentagem = parseFloat(window.prompt('Qual a porcentagem de ajuste, utilize apenas numeros'));
+    var valorAjuste = salario * porcentagem / 100;
+    var novoSalario = salario + valorAjuste;
+    var objSaida = document.querySelector('#saida');
+    var objSaida2 = document.querySelector('#saida2');
+
+    objSaida.style.fontWeight = 'bold';
+    objSaida.style.fontsize = '2em';
+    objSaida.innerHTML = `${nome.toUpperCase()} recebeu um aumento salarial!`
+    objSaida2.innerHTML = `Salario antes do ajuste ${salario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}<br>
+    Valor do ajuste ${valorAjuste.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }<br>
+    Novo salário: ${novoSalario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }`
+
+}
