748afe7325026df1cd23a59870915c454066af40
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-11-08 05:27:11 -0300

    if else

--- moduloD/aula11/codicoes/01codicoes.js
+++ moduloD/aula11/codicoes/01codicoes.js
@@ -1,4 +1,18 @@
 
+function corretor(valor){
+    var nota = valor.value
+    console.log(nota)
+    if(nota < 0 || nota > 10){
+        nota = 1
+    }
+
+}
+
+
+
+
+
+
 function media(nome, b1, b2, b3, b4, b5, saida){
     objNome = nome.value;
     bimestre1 = Number(b1.value);
@@ -7,18 +21,23 @@ function media(nome, b1, b2, b3, b4, b5, saida){
     bimestre4 = Number(b4.value);
     bimestre5 = Number(b5.value);
     objSaida = saida
+    objSaida.style.display = 'block'
     var media = ((bimestre1 + bimestre2 + bimestre3 + bimestre4 + bimestre5) / 5);
-    if(media <= 4){
+
+    if(media <= 5){
         objSaida.style.color =  '#ff0000'
         objSaida.innerHTML = `O aluno(a) ${objNome} recebeu a média final de: ${media}</br><strong>Reprovado!</strong>`
 
     }else if (media >=5 && media <=6){
         objSaida.style.color = '#ff6600'
         objSaida.innerHTML = `O aluno(a) ${objNome} recebeu a média final de: ${media}</br><strong>Recuperação!</strong>`
-    }else{
-        objSaida.style.color = '#007700'
 
+    }else if(media >6 && media <=10){
+        objSaida.style.color = '#007700'
         objSaida.innerHTML = `O aluno(a) ${objNome} recebeu a média final de: ${media}</br><strong>Aprovado!(a)</strong>`
+
+    }else{
+        objSaida.innerHTML = `Verifique os valores digitados`
     }
 
 
