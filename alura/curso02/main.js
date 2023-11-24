9038c77c4ba3fbe2bf1ffd28d81cf31cbbf272c7
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-11-24 06:12:04 -0300

    texto e audio adicionados via função

--- alura/curso02/main.js
+++ alura/curso02/main.js
@@ -1,15 +1,26 @@
 const htmlData = document.querySelector('html');
 
 
-
 const imagem = document.querySelector('.app__image');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
 const btnGeral = document.querySelectorAll('.app__card-button');
-
 const titulo = document.querySelector(".app__title");
-const subtitulo = document.querySelector(".app__title-strong");
+const tocarMusica = document.querySelector("#alternar-musica");
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+
+
+
+tocarMusica.addEventListener("click", () =>{
+    if (tocarMusica.checked == false) {
+        musica.pause();
+    } else {
+        musica.play();
+    }
+})
 
 
 
@@ -17,37 +28,49 @@ const subtitulo = document.querySelector(".app__title-strong");
 function alterarImagem(caminho, objeto, acao){
     imagem.src = caminho;
     htmlData.setAttribute(objeto, acao);
+    for(let i = 0; i < btnGeral.length; i++){
+        btnGeral[i].classList.remove('active');
+    }
+
+    switch(acao){
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            btnFoco.classList.add('active');
+        break;
+        case'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada?,<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            btnCurto.classList.add('active');
+        break;
+
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície,<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            btnLongo.classList.add('active');
+        break;
+        default:
+            break;
+    }
     
 
 }
 
+//Botões para cada uma das telas
+
 btnFoco.addEventListener('click',  () => {
     alterarImagem('imagens/foco.png', 'data-contexto', 'foco');
-    btnGeral[1].classList.remove('active');
-    btnGeral[2].classList.remove('active')
-    btnFoco.classList.add('active');
-    titulo.innerHTML = `Otimize sua produtividade</br>${subtitulo.innerHTML = `mergulhe no que importa.`}`;
     
-
 });
-
 
 
 btnCurto.addEventListener('click', function(){
     alterarImagem('imagens/descanso-curto.png', 'data-contexto', 'descanso-curto');
-    btnGeral[0].classList.remove('active');
-    btnGeral[2].classList.remove('active');
-    btnCurto.classList.add('active');
-    titulo.innerHTML = `Que tal dar uma respirada?</br>${subtitulo.innerHTML = `Faça uma pausa curta!`}`;
-
+    
 });
+
 
 btnLongo.addEventListener('click', function () {
     alterarImagem('imagens/descanso-longo.png', 'data-contexto', 'descanso-longo');
-    btnGeral[0].classList.remove('active');
-    btnGeral[1].classList.remove('active');
-    btnLongo.classList.add('active');
-    titulo.innerHTML = `Hora de voltar a superfície.</br>${subtitulo.innerHTML = `Faça uma pausa longa.`}`
-
 });
 
